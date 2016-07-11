d4456d55411e34fe8bc7e96df67241153d766869
David Ivan <david_ivan@outlook.com>
2016-07-10 20:33:01 -0500

    second commit

--- src/com/pod32g/GA_test/GA.java
+++ src/com/pod32g/GA_test/GA.java
@@ -1,23 +1,57 @@
 package com.pod32g.GA_test;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
 /**
  * Created by pod32g on 05/07/2016.
  */
 public class GA {
+
+    Map<String, String> hashes = new HashMap<String, String>();
+    public GA() {
+        this.hashes.put("+", "0001");
+        this.hashes.put("-","0010");
+        this.hashes.put("*","0011");
+        this.hashes.put("/","0100");
+        this.hashes.put("=","0101");
+    }
+
+    private String convert(String non){
+        String aux = new String();
+        for(int i = 0; i < non.length(); i++){
+            aux += this.hashes.get(""+non.toCharArray()[i]);
+        }
+        return aux;
+    }
+
+    private String generate(){
+        String[] chars = {"+","-","*","/","="};
+        String _final = new String();
+        for (int i = 0; i < 32; i++) {
+            _final += chars[new Random().nextInt(chars.length)];
+        }
+        return _final;
+    }
 
     public static void print(Object aux) {
         System.out.println(aux);
     }
 
     public static void main(String[] args) {
-        FitnessCalc.setSolution("11110000000000000000000000000000000000000000000000000000111111110101101010110110000011100000000000000000000111111111111111111111");
+        GA g = new GA();
+        String solution = g.generate();
+        print("Problem: "+solution);
+        print("Problem in bytes: "+g.convert(solution));
+        FitnessCalc.setSolution(g.convert(solution));
         Population myPop = new Population(50, true);
 
         int generationCount = 0;
 
         while(myPop.getFittest().getFitness() < FitnessCalc.getMaxFitness()){
             generationCount++;
-            print("Generation: " + generationCount + " Fittest: " + myPop.getFittest().getFitness());
+            print("Generation: " + generationCount + " Fitness: " + myPop.getFittest().getFitness());
             myPop = Algorithm.evolvePopulation(myPop);
         }
         print("Solution Found!");
